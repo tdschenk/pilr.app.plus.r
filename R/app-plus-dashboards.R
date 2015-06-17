a27442542ab74c85fa809a8ef0c2074b76e1ca51
Tyler Schenk <schenk.tyler@gmail.com>
2015-06-17 18:20:40 -0500

    revert

--- R/app-plus-dashboards.R
+++ R/app-plus-dashboards.R
@@ -4,23 +4,24 @@
 # REQUIRES XTS
 #' @export
 reported_taken_bar <- function(data, params, ...) {
-  stop(paste0(length(data$dosage), " - ", length(data$survey))
+  dosage <- data$dosage
+  survey <- data$survey
   # Add weekday to dataframe
-  data$survey.metadata.local_time <- as.POSIXlt(data$survey.metadata.local_time, format = "%Y-%m-%dT%H:%M:%SZ")
-  data$survey.data.week <- format(data$survey.metadata.local_time, format = "%W")
+  survey$local_time <- as.POSIXlt(survey$local_time, format = "%Y-%m-%dT%H:%M:%SZ")
+  survey$week <- format(survey$local_time, format = "%W")
   # Unlist columns
-  ##dosage <- as.data.frame(t(apply(dosage, 1, unlist)))
+  dosage <- as.data.frame(t(apply(dosage, 1, unlist)))
   # Remove empty dosage_id rows
-  data <- data[data$dosage.data.id != "", ]
+  dosage <- dosage[dosage$dosage_id != "", ]
   # Find total expected dosages
   dosage_final <- data.frame(dosage_id = character())
-  for (i in 1:length(unique(data$dosage.data.dosage_id))) {
-    dosage_subset <- data[data$dosage.data.dosage_id == 
-                              as.character(unique(data$dosage.data.dosage_id))[i], ]
+  for (i in 1:length(unique(dosage$dosage_id))) {
+    dosage_subset <- dosage[dosage$dosage_id == 
+                              as.character(unique(dosage$dosage_id))[i], ]
     if (as.character(dosage_subset$action[nrow(dosage_subset)]) == "EDIT" |
         as.character(dosage_subset$action[nrow(dosage_subset)]) == "CREATE")
       dosage_final <- rbind(dosage_final, 
-                            data.frame(dosage_id = as.character(unique(data$dosage.data.dosage_id))[i]))
+                            data.frame(dosage_id = as.character(unique(dosage$dosage_id))[i]))
   }
   # Expected doses per week
   expected_doses <- 7*nrow(dosage_final)
@@ -28,8 +29,8 @@ reported_taken_bar <- function(data, params, ...) {
   summary <- data.frame(week_start = character(),
                         percent  = numeric(),
                         category = numeric())
-  for (i in 1:length(unique(data$survey.data.week))) {
-    survey_reported <- data[survey$week == unique(survey$week)[i], ]
+  for (i in 1:length(unique(survey$week))) {
+    survey_reported <- survey[survey$week == unique(survey$week)[i], ]
     prevmonday <- 7 * floor(as.numeric(as.Date(survey_reported$local_time[1])-1+4) / 7) + as.Date(1-4, origin = "1970-01-01") 
     rep_percent <- nrow(survey_reported)/expected_doses
     survey_taken <- survey_reported[survey_reported$response_value == 1, ]
@@ -43,7 +44,7 @@ reported_taken_bar <- function(data, params, ...) {
   }
   summary$week_start <- substr(as.character(summary$week_start), 6, 
                                nchar(as.character(summary$week_start)))
-
+  
   # Bar chart 
   summary %>%
     ggvis(x = ~week_start, y = ~percent, fill = ~category) %>%
@@ -81,7 +82,6 @@ app_plus_table <- function(data, params, ...) {
   mx <- matrix(nrow = length(unique(survey$week)), ncol = 5)
   rownames(mx) <- c(paste0(data$pt[1]))
   colnames(mx) <- c("Week Start", "Reported", "Taken", "Expected", "% Taken")
-  ## Stopped here
   for (i in 1:length(unique(survey$week))) {
     survey_reported <- survey[survey$week == unique(survey$week)[i], ]
     prevmonday <- 7 * floor(as.numeric(as.Date(survey_reported$local_time[1])-1+4) / 7) + as.Date(1-4, origin = "1970-01-01") 
